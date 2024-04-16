76b4c171101bdab247c22eb050d10370cbc4aa27
Cuauhtémoc Gerardo Montoya Reséndez <144742771+Cuauhtemoc45@users.noreply.github.com>
2024-04-16 11:25:09 -0600

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const MONGO_URL = "mongodb+srv://gerardoresendez040:cono5000@cluster0.i5d6vtd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const MONGO_URL = "mongodb+srv://gerardoresendez040:cono5000@cluster0.i5d6vtd.mongodb.net/";
 
 export const connectDB = async () => {
   try {
