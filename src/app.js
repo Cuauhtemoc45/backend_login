03f0ede287603abec1db2059d30abb15842b582a
Cuauhtémoc Gerardo Montoya Reséndez <144742771+Cuauhtemoc45@users.noreply.github.com>
2024-04-16 11:35:47 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://backend-login-ai13.onrender.com",
     // preflightContinue: true,
     credentials: true,
   })
